ba4466de247ad4e0bad417c7da12879c7ae40d00
Thien luu duc <tntbv198@gmail.com>
2026-05-21 20:59:03 +0700

    Update main.py

--- Idle Incremental/main.py
+++ Idle Incremental/main.py
@@ -1,12 +1,14 @@
-import pygame,numpy
+import pygame,numpy,os
+from pygame.locals import * #type:ignore
 from sys import exit
+os.system("pip install pygame-ce")
 pygame.init()
 info=pygame.display.Info()
 width=info.current_w
 height=info.current_h
 screen=pygame.display.set_mode((width,height), pygame.RESIZABLE)
 pygame.display.set_caption("Idle Incremental")
-font=pygame.font.Font("Idle Incremental/BalsamiqSans-Bold.ttf",30)
+block=Rect(width-300,50,250,75)
 red=(255,0,0)
 orange=(255,128,0)
 yellow=(255,155,0)
@@ -17,20 +19,39 @@ blue=(0,0,255)
 purple=(255,0,128)
 pink=(255,0,255)
 white=(255,255,255)
-tick=0
-def text(text,color,x,y):
+buy=pygame.mixer.Sound("Idle Incremental/Activate.mp3")
+click=False
+def text(size,text,color,x,y):
+    global font
+    font=pygame.font.Font("Idle Incremental/BalsamiqSans-Bold.ttf",size)
     img=font.render(text,True,color)
     screen.blit(img,(x,y))
+def drawrectandtext():
+    pygame.draw.rect(screen,red,block,border_radius=15)
+    text(50,"Welcome to Idle Incremental!",(255,255,255),500,360)
+    text(24,"Click me to buy!",(255,255,255),1105,75)
+def rectcollidemouse():
+    global click
+    if event.type==pygame.MOUSEBUTTONDOWN and not click:
+        click=True
+    if event.type==pygame.MOUSEBUTTONUP and click:
+        click=False
+        if block.collidepoint(pos):
+            buy.play()
 def draw():
     screen.fill("#272528")
+    drawrectandtext()
+def main():
+    draw()
+    rectcollidemouse()
 fps,clock=60,pygame.time.Clock()
 run=True
 while run:
+    pos=pygame.mouse.get_pos()
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             run=False
-    draw()
-    text("Hello World!",(255,255,255),100,100)
+    main()
     clock.tick(fps)
     pygame.display.update()
 pygame.quit()
